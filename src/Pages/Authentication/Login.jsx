3fcc10db607cb1be5772fd0d5ea458de7be7410e
ridoy <mdrezuanislamridoy@gmail.com>
2024-04-04 13:03:01 +0600

    kire

--- src/Pages/Authentication/Login.jsx
+++ src/Pages/Authentication/Login.jsx
@@ -16,29 +16,34 @@ const Login = () => {
     Authenticaion('login',credential)
   }
   return (
-    <div className="flex justify-center">
-      <div className="flex w-4/5 justify-center ">
-        <div className="w-2/4 md:hidden sm:hidden lg:block"><img src="img/image.png" alt="" /></div>
-        <form action="" className="border w-2/4 md:w-96 sm:w-96 p-5 ">
-          <table>
-            <tr >
-              <td className="w-1/2"> <label htmlFor="">Enter email</label></td>
-              <td className="w-1/2"><input type="text" className="border" onChange={(e)=>{ setEmail(e.target.value)}}/></td>
-            </tr>
-           
-            <tr >
-              <td className="w-1/2"> <label htmlFor="">Enter password</label></td>
-              <td className="w-1/2"><input type="password" className="border" onChange={(e)=>{ setPassword(e.target.value)}}/></td>
-            </tr>
-          
-          </table>
-          <button onClick={handleLogin}> Log in </button>
-          <div>
-              <p>New here? <Link to="/auth/signup">Sign up </Link></p>
-          </div>
-        </form>
+    <div className="flex bg-gradient-to-r from-blue-300 to-cyan-500 lg:h-screen sm:h-screen md:h-screen items-center justify-center">
+      
+      <div className="flex p-4 bg-white h-3/5 items-center w-3/5 justify-center ">
+        <div className="w-2/4 flex justify-center md:hidden sm:hidden lg:block">
+          <img className="w-96" src={image} alt="" />
+        </div>
+        <div className=" lg:w-2/4 flex justify-center">
+          <form action="" className=" md:w-96 sm:w-96  p-5">
+            <table>
+              <tr className="my-1">
+                <td className="w-1/2"> <label htmlFor="">Enter email</label></td>
+                <td className="w-1/2"><input type="text" className="border" onChange={(e)=>{ setEmail(e.target.value)}}/></td>
+              </tr>
+            
+              <tr className="my-1">
+                <td className="w-1/2"> <label htmlFor="">Enter password</label></td>
+                <td className="w-1/2"><input type="password" className="border" onChange={(e)=>{ setPassword(e.target.value)}}/></td>
+              </tr>
+            
+            </table>
+            <button onClick={handleLogin}> Log in </button>
+            <div>
+                <p>New here? <Link to="/auth/signup">Sign up </Link></p>
+            </div>
+          </form>
+        </div>
       </div>
-</div>
+    </div>
   )
 }
 export default Login
