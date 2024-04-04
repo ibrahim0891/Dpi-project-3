2793a4706f9b95d50c76340a3024842f493aeeac
ridoy <mdrezuanislamridoy@gmail.com>
2024-04-04 18:55:48 +0600

    sign up

--- src/Pages/Authentication/Signup.jsx
+++ src/Pages/Authentication/Signup.jsx
@@ -24,17 +24,17 @@ const SignUp = () => {
         Authenticaion('signup',credential, navigate) 
     }
     return (
-        <form action="" className="border  px-8 py-12 m-auto">
+        <form action="" className="border  px-8 py-12 m-auto ">
             <h1 className="font-thin text-3xl text-center"> সাক্ষর উপরে করেন </h1> <br/>
-            <label className="py-2 font-bold text-md" htmlFor="">First name</label> <br/>
-            <input className="border-2 p-2" type="text" onChange={(e)=>{setFname(e.target.value)}} /> <br/>
-            <label className="py-2 font-bold text-md" htmlFor="">Last name</label> <br/>
-            <input className="border-2 p-2" type="text"  onChange={(e)=>{setLname(e.target.value)}}/> <br/>
-            <label className="py-2 font-bold text-md" htmlFor="">Email Address</label> <br/>
-            <input className="border-2 p-2" type="email"  onChange={(e)=>{setEmail(e.target.value)}}/> <br/>
-            <label className="py-2 font-bold text-md" htmlFor="">Password</label> <br/>
-            <input className="border-2 p-2" type="password"  onChange={(e)=>{setPassword(e.target.value)}}/> <br/>
-            <button onClick={(e)=>handleSignup(e)}> Signup </button>
+            <label className="my-3 w-full py-2 font-bold text-md" htmlFor="">First name</label> <br/>
+            <input className="my-3 w-full border-2 p-2" type="text" onChange={(e)=>{setFname(e.target.value)}} /> <br/>
+            <label className="my-3 w-full py-2 font-bold text-md" htmlFor="">Last name</label> <br/>
+            <input className="my-3 w-full border-2 p-2" type="text"  onChange={(e)=>{setLname(e.target.value)}}/> <br/>
+            <label className="my-3 w-full py-2 font-bold text-md" htmlFor="">Email Address</label> <br/>
+            <input className="my-3 w-full border-2 p-2" type="email"  onChange={(e)=>{setEmail(e.target.value)}}/> <br/>
+            <label className="my-3 w-full py-2 font-bold text-md" htmlFor="">Password</label> <br/>
+            <input className="my-3 w-full border-2 p-2" type="password"  onChange={(e)=>{setPassword(e.target.value)}}/> <br/>
+            <button  className="block bg-gray-800 text-white hover:bg-gray-700 px-3 py-2 my-4" onClick={(e)=>handleSignup(e)}> Signup </button>
             <div>
                 Already have account? <Link to="/auth/login">Log in</Link>
             </div>
