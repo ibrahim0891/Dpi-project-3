8a7fa9845178253249d0da81496545cc0113d88e
ridoy <mdrezuanislamridoy@gmail.com>
2024-04-04 11:23:49 +0600

    good night

--- src/Pages/Authentication/Login.jsx
+++ src/Pages/Authentication/Login.jsx
@@ -16,18 +16,28 @@ const Login = () => {
     Authenticaion('login',credential)
   }
   return (
-    <div className="flex w-4/5 justify-center ">
-      <div className="w-2/4 "><img src="img/image.png" alt="" /></div>
-      <form action="" className="border w-2/4 p-5 ">
-        <label htmlFor="">Enter email</label>
-        <input type="text" className="border" onChange={(e)=>{ setEmail(e.target.value)}}/><br/>
-        <label htmlFor="">Enter password</label>
-        <input type="password" onChange={(e)=>{ setPassword(e.target.value)}}/>
-        <button onClick={handleLogin}> Log in </button>
-        <div>
-            <p>New here? <Link to="/auth/signup">Sign up </Link></p>
-        </div>
-      </form>
+    <div className="flex justify-center">
+      <div className="flex w-4/5 justify-center ">
+        <div className="w-2/4 md:hidden sm:hidden lg:block"><img src="img/image.png" alt="" /></div>
+        <form action="" className="border w-2/4 md:w-96 sm:w-96 p-5 ">
+          <table>
+            <tr >
+              <td className="w-1/2"> <label htmlFor="">Enter email</label></td>
+              <td className="w-1/2"><input type="text" className="border" onChange={(e)=>{ setEmail(e.target.value)}}/></td>
+            </tr>
+           
+            <tr >
+              <td className="w-1/2"> <label htmlFor="">Enter password</label></td>
+              <td className="w-1/2"><input type="password" className="border" onChange={(e)=>{ setPassword(e.target.value)}}/></td>
+            </tr>
+          
+          </table>
+          <button onClick={handleLogin}> Log in </button>
+          <div>
+              <p>New here? <Link to="/auth/signup">Sign up </Link></p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
