726bc2cc87f003da8269fba3e64a0da1df769367
ridoy <mdrezuanislamridoy@gmail.com>
2024-04-08 19:28:59 +0600

    good night

--- src/Pages/Primary/Chat/Inbox.jsx
+++ src/Pages/Primary/Chat/Inbox.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom"
 import { links } from "../../../assets/Vars"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
 //under development
 const Inbox = () => {
@@ -8,9 +9,9 @@ const Inbox = () => {
         <div>
             <h1> Messaging page </h1>
             <div className="flex flex-col px-4 py-2 ">
-                <Link className="p-4" to={links.sec.modInbox + 1}> Thread 1 </Link>
-                <Link className="p-4" to={links.sec.modInbox + 2}> Thread 2 </Link>
-                <Link className="p-4" to={links.sec.modInbox + 3}> Thread 3</Link>
+                <Link className="p-4 flex items-center" to={links.sec.modInbox + 1}><div className="w-8 h-8 mr-2 border rounded-full flex justify-center items-center overflow-hidden"><FontAwesomeIcon className="text-2xl " icon={faUser} /></div> Thread 1 </Link>
+                <Link className="p-4 flex items-center" to={links.sec.modInbox + 2}><div className="w-8 h-8 mr-2 border rounded-full flex justify-center items-center overflow-hidden"><FontAwesomeIcon className="text-2xl " icon={faUser} /></div> Thread 2 </Link>
+                <Link className="p-4 flex items-center" to={links.sec.modInbox + 3}><div className="w-8 h-8 mr-2 border rounded-full flex justify-center items-center overflow-hidden"><FontAwesomeIcon className="text-2xl " icon={faUser} /></div> Thread 3</Link>
             </div>
         </div>
     )
