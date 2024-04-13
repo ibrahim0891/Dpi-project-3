71890966a476929c5dbf6fb656e4a22d2547f2d7
ibrahim0891 <nijhum0891@gmail.com>
2024-04-13 20:38:47 +0600

    thread Security issue fixed

--- src/Pages/Authentication/Login.jsx
+++ src/Pages/Authentication/Login.jsx
@@ -4,7 +4,9 @@ import { Authenticaion } from "../../Common/Authfunction"
 import image from '../../assets/img/loginbg.png'
 
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth"
-import { writeDataInDB } from "../../Common/Database"
+import { writeDataInDB } from "../../Common/Database" 
+import googleLogo from '../../assets/img/google-logo.png';
+
 //Ready
 
 const Login = () => {
@@ -16,10 +18,10 @@ const Login = () => {
         const provider = new GoogleAuthProvider()
         const auth = getAuth()
         signInWithPopup(auth, provider).then((result) => {
-            writeDataInDB('/users/'+result.user.uid+'/info' , {
+            writeDataInDB('/users/' + result.user.uid + '/info', {
                 email: result.user.email,
                 fname: result.user.displayName,
-                avater : result.user.photoURL
+                avater: result.user.photoURL
             })
         }).catch((error) => {
             console.log(error, error.code);
@@ -56,7 +58,12 @@ const Login = () => {
                             <button onClick={(e) => closeError(e)} className="px-2 cursor-pointer">ok</button>
                         </p> : ''}
                 </div>
-                <div className=" flex flex-col">
+                <div className="py-4 text-center cursor-pointer flex items-center justify-center rounded-lg shadow-md" onClick={googleSignIn}>
+                    <img src={googleLogo} className="w-5 mr-2" alt="" />
+                    <span className="text-md ">Sign in with Google</span>
+                </div>
+                <div className="text-center py-4 text-lg"> or </div>
+                <div className=" flex flex-col p-6 shadow-md border rounded-md">
                     <label className="py-2 font-bold text-md" htmlFor="">Enter email</label>
                     <input
                         type="text"
@@ -74,12 +81,10 @@ const Login = () => {
                     <button className="block bg-gray-800 text-white hover:bg-gray-700 px-3 py-2 my-4" onClick={handleLogin}> Log in </button>
                 </div>
 
-                <div className="pt-4 text-center">
+                <div className="py-4 mt-4 text-center shadow-md border rounded-md">
                     <p className="">New here? <Link className="text-blue-600 hover:underline " to="/auth/signup">Sign up </Link></p>
                 </div>
-                <div className="pt-4 text-center" onClick={googleSignIn}>
-                    <span> Sign in with google</span>
-                </div>
+
             </form>
         </div>
     )
--- src/Pages/Secondary/thread.jsx
+++ src/Pages/Secondary/thread.jsx
@@ -19,12 +19,14 @@ const ChatView = () => {
 
     const autoScroller = useRef(null)
 
-    let genarateThreadID = (uid1, uid2) => {
-        let sortedUID = [uid1, uid2].sort()
-        let combinedUID = sortedUID.join('')
-        return combinedUID.slice(0, 6)
+    let generateThreadID = (uid1, uid2) => {
+        let combinedUID = [uid1.slice(0, 4), uid2.slice(0, 4)].sort().join('').slice(0, 8);
+        return combinedUID;
     }
-    let threadID = genarateThreadID(auth.currentUser.uid, chatIDnumber)
+    
+    
+    let threadID = generateThreadID(auth.currentUser.uid, chatIDnumber)
+    console.log(threadID);
     let message = {
         sender: auth.currentUser.uid,
         message: messageContent,
@@ -36,7 +38,6 @@ const ChatView = () => {
         get(child(ref(database), `/users/${chatIDnumber}/info`)).then((snapshot) => {
             setReceiver(snapshot.val())
         })
-
         onValue(ref(database, '/chats/' + threadID), (snapshot) => {
             if (snapshot.exists()) {
                 setMessages(snapshot.val())
@@ -81,13 +82,15 @@ const ChatView = () => {
             {receiver ? <div className="flex flex-col h-fit bg-green-200 justify-between ">
                 <BackButton titlebarText={"Messaging to " + receiver.fname } buttonLink={links.home.inbox.chatLayout} />
 
-                <div className=" bg-gray-50 " >
+                <div className=" bg-gray-50 min-h-[550px] " >
                     {messsages ? Object.keys(messsages).map((objKeys) =>
                         <div className={messsages[objKeys].sender == chatIDnumber ? '' : 'text-right'} key={objKeys}>
-                            <p className="px-4 py-2 m-2 rounded-sm inline-block bg-slate-200">{messsages[objKeys].message}</p>
+                            <p className="px-4 py-2 m-2 rounded-sm inline-block bg-slate-200">{messsages[objKeys].message}
+                            
+                            {messsages ? <div ref={autoScroller}> </div> :  null }
+                            </p>
                         </div>
                     ) : firstMessage ? firstMessage : 'Loading...'}
-                    {messsages ? <div ref={autoScroller}></div> :  null }
                 </div>
                 <form className="flex bg-gray-200 items-center justify-between sticky bottom-0">
                     <textarea className="resize-none border w-full" value={messageContent} onChange={(e) => setMessageContent(e.target.value)} onKeyDown={handleKeyDown}></textarea>
