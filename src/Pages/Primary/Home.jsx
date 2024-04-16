64b8488f0c53a3a7cab7e80c0e9a6b7b22aefa9c
ridoy <mdrezuanislamridoy@gmail.com>
2024-04-16 11:42:51 +0600

    add profile pic

--- src/Pages/Primary/Home.jsx
+++ src/Pages/Primary/Home.jsx
@@ -3,11 +3,16 @@
 let Home = () => {
   return (
     <div>
-      <div className="post">
-
+      <div className="post m-4 flex justify-between items-center">
+        <div className=" w-8 h-8  m-3 border rounded-full flex justify-center items-center overflow-hidden bg-white">
+          <img src={userData.avater} alt="" className=""/>
+        </div>
+        <div >
+          <input type="text" className="w-full p-2 outline-none" placeholder="What's on your mind?"/>
+        </div>
       </div>
       <div className="body">
-        
+
       </div>
     </div>
   )
