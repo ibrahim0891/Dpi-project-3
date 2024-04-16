6bcf10f9eae2867f49d489d04ba62896632ced94
ridoy <mdrezuanislamridoy@gmail.com>
2024-04-16 12:22:34 +0600

    ok

--- src/Pages/Primary/Home.jsx
+++ src/Pages/Primary/Home.jsx
@@ -15,12 +15,12 @@ let Home = () => {
     }, []);
   return (
     <div>
-      <div className="post m-4 flex justify-between items-center">
-        <div className=" w-8 h-8  m-3 border rounded-full flex justify-center items-center overflow-hidden bg-white">
-         {userData && <img src={userData.avater} alt="" className=""/>} 
+      <div className="post m-4 flex items-center">
+        <div className=" w-1/12  m-3  flex justify-center items-center overflow-hidden ">
+         {userData && <img src={userData.avater} alt="" className="w-8 rounded-full h-8"/>} 
         </div>
-        <div >
-          <input type="text" className="w-full p-2 outline-none" placeholder="What's on your mind?"/>
+        <div className="w-10/12">
+          <div className="w-full p-2 flex items-center cursor-pointer bg-slate-200 border rounded-full h-8">What is on your mind?</div>
         </div>
       </div>
       <div className="body">
